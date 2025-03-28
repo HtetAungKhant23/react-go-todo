e23a6a7e2e5b445ac413f87406ec8804f14599a9
Htet Aung Khant <htetaung23.ha@gmail.com>
2025-03-29 00:01:23 +0630

    feat(app): healthcheck route

--- main.go
+++ main.go
@@ -11,5 +11,9 @@ func main() {
 	fmt.Println("Hi gophers!")
 	app := fiber.New()
 
+	app.Get("/health-check", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "server still alive."})
+	})
+
 	log.Fatal(app.Listen(":4000"))
 }
